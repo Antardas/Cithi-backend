12bd90aefc72498b99ee8ffb752cc91668a4f859
Antar <antardas2334@gmail.com>
2023-08-02 23:27:00 +0600

    added some helpers function

---
+++ src/shared/global/helpers/helpers.ts
@@ -0,0 +1,25 @@
+export class Helpers {
+  static firstLatterUpperCase(str: string): string {
+    const valueString = str.toLowerCase();
+    return valueString
+      .split(' ')
+      .map((value: string) => `${value.charAt(0).toUpperCase()}${value.slice(1).toLowerCase()}`)
+      .join(' ');
+  }
+
+  static lowerCase(str: string): string {
+    return str.toLowerCase();
+  }
+
+  static generateRandomIntegers(integerLength: number): number {
+    const characters = '0123456789';
+    let results = '';
+    const characterLength = characters.length;
+
+    for (let i = 0; i < integerLength; i++) {
+      results += characters.charAt(Math.floor(Math.random() * characterLength));
+    }
+
+    return parseInt(results, 10);
+  }
+}
