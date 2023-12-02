a76ffa85d34b17c9180b66415d341df9fef13508
Antar Das <antardas2334@gmail.com>
2023-12-02 14:42:34 +0600

    For notification set socket

--- src/shared/sockets/post.ts
+++ src/shared/sockets/post.ts
@@ -1,4 +1,6 @@
 import { Server, Socket } from 'socket.io';
+import { IReactionDocument } from '@/reaction/interfaces/reaction.interface';
+import { ICommentDocument } from '@/comment/interfaces/comment.interface';
 
 export let socketIOPostObject: Server;
 
@@ -7,12 +9,19 @@ export class SockIOPostHandler {
   constructor(io: Server) {
     this.io = io;
     socketIOPostObject = io;
-
   }
 
   public listen(): void {
-    this.io.on('connection', (_socket: Socket) => {
+    this.io.on('connection', (socket: Socket) => {
       console.log('Post Socket IO handler');
+
+      socket.on('reaction', (reaction: IReactionDocument) => {
+        this.io.emit('update like', reaction);
+      });
+
+      socket.on('comment', (comment: ICommentDocument) => {
+        this.io.emit('update comment', comment);
+      });
     });
   }
 }
