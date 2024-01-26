e8f27f6178eefcadd87b568c0b49ca7715cfed29
Antar Das <antardas2334@gmail.com>
2024-01-26 10:36:02 +0600

    Added Block and Unblock user service

--- src/shared/services/redis/follower.cache.ts
+++ src/shared/services/redis/follower.cache.ts
@@ -1,11 +1,12 @@
 import { UserCache } from '@/service/redis/user.cache';
 import { IFollowerData } from '@/follower/interfaces/follower.interface';
-import { ServerError } from '@/global/helpers/error-handler';
+import { NotFoundError, ServerError } from '@/global/helpers/error-handler';
 import { config } from '@/root/config';
 import { BaseCache } from '@/service/redis/base.cache';
 import Logger from 'bunyan';
 import { IUserDocument } from '@/user/interfaces/user.interface';
 import mongoose from 'mongoose';
+import { Helpers } from '@/global/helpers/helpers';
 
 const log: Logger = config.createLogger('FollowerCache');
 const userCache: UserCache = new UserCache();
@@ -81,6 +82,31 @@ export class FollowerCache extends BaseCache {
         }
       }
       return followers;
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try Again');
+    }
+  }
+
+  public async updateBlockedPropInCache(userId: string, blockUserId: string, prop: string, type: 'block' | 'unblock'): Promise<void> {
+    // TODO:  blockedBy need to update
+    try {
+      await this.createConnection();
+      const blockedString: string = (await this.client.HGET(`users:${userId}`, prop)) as string;
+      const blocked: string[] = Helpers.parseJson(blockedString);
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+
+      if (type === 'block') {
+        blocked.push(blockUserId);
+      } else {
+        const blockUserIndex: number = blocked.findIndex((id) => id === blockUserId);
+        if (blockUserIndex === -1) {
+          throw new NotFoundError('Block user not found');
+        }
+        blocked.splice(blockUserIndex, 1);
+      }
+      multi.HSET(`users:${userId}`, `${prop}`, JSON.stringify(blocked));
+      await multi.exec();
     } catch (error) {
       log.error(error);
       throw new ServerError('Server error. Try Again');
