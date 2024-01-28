9a1805577f6ab6e660df530c9aa5e55cc53c6440
Antar Das <antardas2334@gmail.com>
2024-01-28 07:49:57 +0600

    Added Email Queue

--- src/shared/services/queues/email.queue.ts
+++ src/shared/services/queues/email.queue.ts
@@ -1,11 +1,12 @@
 import { IEmailJob } from '@/user/interfaces/user.interface';
 import { BaseQueue } from './base.queue';
 import { emailWorker } from '@/worker/email.worker';
-
+export const COMMENT_EMAIL: string = 'COMMENT_EMAIL';
 class EmailQueue extends BaseQueue {
   constructor() {
     super('emails');
     this.processJob('forgetPasswordEmail', 5, emailWorker.addNotificationEmail);
+    this.processJob(COMMENT_EMAIL, 5, emailWorker.addNotificationEmail);
   }
 
   public addEmailJob(name: string, data: IEmailJob): void {
