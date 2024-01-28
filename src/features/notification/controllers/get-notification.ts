082b30bc900b2a2ded33210bb9eed9dc2d85eeb6
Antar Das <antardas2334@gmail.com>
2024-01-28 16:55:16 +0600

    :sparkles: Added notificaiton routes and contorller
    Get

---
+++ src/features/notification/controllers/get-notification.ts
@@ -0,0 +1,14 @@
+import { INotificationDocument } from '@/notification/interfaces/notification.interface';
+import HTTP_STATUS from 'http-status-codes';
+import { Request, Response } from 'express';
+import { notificationService } from '@/service/db/notification.service';
+export class Get {
+  public async notification(req: Request, res: Response): Promise<void> {
+    const notifications: INotificationDocument[] = await notificationService.getNotifications(`${req.currentUser?.userId}`);
+
+    res.status(HTTP_STATUS.OK).json({
+      data: notifications,
+      message: 'All Notifications'
+    });
+  }
+}
