2d94ff93b33fbc839e8c634901c1df37f365c1bb
Antar Das <antardas2334@gmail.com>
2024-01-29 21:11:09 +0600

    rename the job

--- src/shared/services/queues/image.queue.ts
+++ src/shared/services/queues/image.queue.ts
@@ -4,7 +4,7 @@ import { imageWorker } from '@/worker/image.worker';
 
 export const ADD_IMAGE_TO_DB: string = 'ADD_IMAGE_TO_DB';
 export const ADD_USER_PROFILE_IMAGE_TO_DB: string = 'ADD_USER_PROFILE_IMAGE_TO_DB';
-export const UPDATE_IMAGE_IN_BD: string = 'UPDATE_IMAGE_IN_BD';
+export const UPDATE_BACKGROUND_IMAGE_IN_BD: string = 'UPDATE_BACKGROUND_IMAGE_IN_BD';
 export const REMOVE_IMAGE_FROM_DB: string = 'REMOVE_IMAGE_FROM_DB';
 
 class ImageQueue extends BaseQueue {
@@ -12,7 +12,7 @@ class ImageQueue extends BaseQueue {
     super('Image');
     this.processJob(ADD_IMAGE_TO_DB, 5, imageWorker.addImageToDB);
     this.processJob(ADD_USER_PROFILE_IMAGE_TO_DB, 5, imageWorker.addUserProfileImageToDB);
-    this.processJob(UPDATE_IMAGE_IN_BD, 5, imageWorker.updateBgImageInDB);
+    this.processJob(UPDATE_BACKGROUND_IMAGE_IN_BD, 5, imageWorker.updateBgImageInDB);
     this.processJob(REMOVE_IMAGE_FROM_DB, 5, imageWorker.removeImageFromDB);
   }
 
