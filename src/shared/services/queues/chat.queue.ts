30a443e72cd084baa89c2e3f8e7f8e2cb73e44ab
Antar Das <antardas2334@gmail.com>
2024-02-07 11:31:48 +0600

    Added Worker and Queue

--- src/shared/services/queues/chat.queue.ts
+++ src/shared/services/queues/chat.queue.ts
@@ -5,11 +5,13 @@ import { authWorker } from '@/worker/auth.worker';
 import { chatWorker } from '@/worker/chat.worker';
 export const ADD_CHAT_TO_DB: string = 'ADD_CHAT_TO_DB';
 export const MARK_MESSAGE_AS_DELETED: string = 'MARK_MESSAGE_AS_DELETED';
+export const MARK_MESSAGE_AS_READ: string = 'MARK_MESSAGE_AS_READ';
 class ChatQueue extends BaseQueue {
   constructor() {
     super('chat');
     this.processJob(ADD_CHAT_TO_DB, 5, chatWorker.addMessageToDB);
     this.processJob(MARK_MESSAGE_AS_DELETED, 5, chatWorker.markMessageAsDeleted);
+    this.processJob(MARK_MESSAGE_AS_READ, 5, chatWorker.markMessageAsRead);
   }
   public addChatJob(name: string, data: IMessageData | IMessageJobData) {
     this.addJob(name, data);
