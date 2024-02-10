7b6ef79c50352eea4123af6df8d9fd5c29444621
Antar Das <antardas2334@gmail.com>
2024-02-11 01:49:30 +0600

    Added Get Multiple users from cache with pagination

--- src/shared/services/redis/user.cache.ts
+++ src/shared/services/redis/user.cache.ts
@@ -4,8 +4,12 @@ import { config } from '@/root/config';
 import { BaseCache } from '@/service/redis/base.cache';
 import { INotificationSettings, ISocialLinks, IUserDocument } from '@/user/interfaces/user.interface';
 import Logger from 'bunyan';
+import { RedisCommandRawReply } from '@redis/client/dist/lib/commands';
 const log: Logger = config.createLogger('UserCache');
 type UserItem = string | number | ISocialLinks | INotificationSettings;
+
+type UserCacheMultiType = string | number | Buffer | RedisCommandRawReply[] | IUserDocument | IUserDocument[];
+
 export class UserCache extends BaseCache {
   constructor() {
     super('UserCache');
@@ -126,6 +130,42 @@ export class UserCache extends BaseCache {
       response.bgImageId = Helpers.parseJson(`${response.bgImageId}`);
       response.bgImageVersion = Helpers.parseJson(`${response.bgImageVersion}`);
       return response;
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try Again!!');
+    }
+  }
+
+  public async getUsersFromCache(start: number = 0, end: number = 10, excludedUserId: string): Promise<IUserDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const usersIdList: string[] = await this.client.ZRANGE('user', start, end);
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+      for (const userId of usersIdList) {
+        if (userId !== excludedUserId) {
+          multi.HGETALL(`users:${userId}`);
+        }
+      }
+      const multiResponse: UserCacheMultiType = (await multi.exec()) as UserCacheMultiType;
+      const usersDocuments: IUserDocument[] = [];
+
+      for (const user of multiResponse as IUserDocument[]) {
+        user.createdAt = new Date(Helpers.parseJson(`${user.createdAt}`));
+        user.postsCount = Helpers.parseJson(`${user.postsCount}`);
+        user.blocked = Helpers.parseJson(`${user.blocked}`);
+        user.blockedBy = Helpers.parseJson(`${user.blockedBy}`);
+        user.notifications = Helpers.parseJson(`${user.notifications}`);
+        user.social = Helpers.parseJson(String(user.social));
+        user.followersCount = Helpers.parseJson(`${user.followersCount}`);
+        user.followingCount = Helpers.parseJson(`${user.followingCount}`);
+        user.bgImageId = Helpers.parseJson(`${user.bgImageId}`);
+        user.bgImageVersion = Helpers.parseJson(`${user.bgImageVersion}`);
+        usersDocuments.push(user);
+      }
+      return usersDocuments;
     } catch (error) {
       log.error(error);
       throw new ServerError('Server error. Try Again!!');
