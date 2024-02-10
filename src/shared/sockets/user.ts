a58f954271255a70d0c17487f3d32b433ea3dd47
Antar Das <antardas2334@gmail.com>
2024-02-10 18:08:01 +0600

    Connect and Disconnect socket

--- src/shared/sockets/user.ts
+++ src/shared/sockets/user.ts
@@ -1,7 +1,10 @@
-import { ISocketData } from '@/user/interfaces/user.interface';
+import { ILogin, ISocketData } from '@/user/interfaces/user.interface';
 import { Server, Socket } from 'socket.io';
-let socketIoUserObject: Server;
 
+export let socketIoUserObject: Server;
+export const connectedUsersMap: Map<string, string> = new Map();
+
+const users: string[] = [];
 export class SocketIoUserHandler {
   private io: Server;
   constructor(io: Server) {
@@ -17,6 +20,31 @@ export class SocketIoUserHandler {
       socket.on('UNBLOCK_USER', (data: ISocketData) => {
         this.io.emit('UNBLOCK_USER_ID', data);
       });
+
+      socket.on('SETUP', (data: ILogin) => {
+        this.addClientToMap(data.userId, socket.id);
+      });
+
+      socket.on('DISCONNECT', () => {
+        this.removeClientFromMap(socket.id);
+      });
     });
+  }
+
+  private addClientToMap(userId: string, socketId: string): void {
+    if (!connectedUsersMap.has(userId)) {
+      connectedUsersMap.set(userId, socketId);
+    }
+  }
+
+  private removeClientFromMap(socketId: string) {
+    if (Array.from(connectedUsersMap.values()).includes(socketId)) {
+      const disconnectedUser: [string, string] = [...connectedUsersMap].find((user: [string, string]) => {
+        return user[1] === socketId;
+      }) as [string, string];
+
+      connectedUsersMap.delete(disconnectedUser[0]);
+      // TODO: send event to the client
+    }
   }
 }
