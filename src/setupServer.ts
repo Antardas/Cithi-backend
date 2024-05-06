571d86a7c7f425d3af442af8a09b322adbfc046f
Antar <antardas2334@gmail.com>
2024-05-07 00:24:33 +0600

    fixed cors issue

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -56,7 +56,7 @@ export class ChattyServer {
     app.use(helmet());
     app.use(
       cors({
-        origin: [config.CLIENT_URL, 'http://localhost:5173', '*'] as string[], // TODO: make it actual origin in production
+        origin: [config.CLIENT_URL, '*'] as string[], // TODO: make it actual origin in production
         credentials: true,
         optionsSuccessStatus: 200,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
