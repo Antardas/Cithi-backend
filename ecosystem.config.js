e36233b92fda6f61245f812030512497f26558b6
Antar <antardas2334@gmail.com>
2024-06-23 19:38:37 +0600

    specify the port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
     name: 'Chithi',
     script: './build/app.js',
     env: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
+      PORT: 5000
     },
     instances: 5, watch: true, ignore_watch: ['./storage/*', '.git/*'], attach: true
   }]
